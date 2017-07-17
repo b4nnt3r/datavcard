473174f46cc030da4ac1ab48e3595bef2fa95f86
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-17 15:55:25 -0400

    needs styling

---
+++ main.js
@@ -0,0 +1,38 @@
+let req = new XMLHttpRequest();
+req.addEventListener("load", place);
+req.open('GET', 'https://api.github.com/users/b4nnt3r');
+req.send();
+
+function place() {
+  let data = JSON.parse(this.responseText);
+
+  let template = `
+  <div class="title">
+  <h1>${data.name}</h1>
+  </div>
+  <article>
+    <div class="me">
+      <h2>The Basics</h2>
+      <p>Name: </p>${data.name}<br>
+      <p>Github URL: </p><a href=${data.html_url}>${data.login}</a><br>
+      <p>ID: </p>${data.id}<br>
+      <p>Company: </p>${data.company}<br>
+      <p>Website:</p> <a href=${data.blog}>${data.blog}</a><br>
+    </div>
+    <div class="story">
+      <h2>The Story</h2>
+      <div class="bio">
+      ${data.bio}
+      </div>
+    </div>
+    <div class="image">
+    <img src="${data.avatar_url}">
+    </div>
+
+  </article>
+  `;
+
+  document.querySelector('.cont').innerHTML += template;
+
+  console.log(req);
+}
